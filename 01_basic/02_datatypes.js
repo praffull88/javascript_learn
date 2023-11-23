59afdf2eef54b96895219d0822f104865f3f12c3
PRAFFULL TEMBHURNE <104838725+praffull88@users.noreply.github.com>
2023-11-23 08:26:02 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+//alert (3+3) we are using node js, not browser  
+
+console.log(3+3) // code readability is very high
+
+console.log("praffull")
+
+let name = "praffull"
+let age = 15
+let isLoggedIn = false
+
+/*
+    number => 2 to power 53
+    bigint 
+    string=> ""
+    boolean => true/false
+    null => standalone value
+    undefined => 
+    symbol => unique
+
+    object
+    
+*/
+
+console.log(typeof undefined);
